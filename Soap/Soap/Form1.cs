b473e8753a42e98d931de1d213eb11291900aed4
Alex Levente Socosan <alex.socosan@stud.uni-corvinus.hu>
2021-10-27 12:16:28 +0200

    Webszolg hívása és Árfolyam lista létrehozása

--- Soap/Soap/Form1.cs
+++ Soap/Soap/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Soap.Entities;
+using Soap.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,25 @@ namespace Soap
 {
     public partial class Form1 : Form
     {
+        BindingList<RateDate> Rates = new BindingList<RateDate>();
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
+        }
+        private void Webszolghívása()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
