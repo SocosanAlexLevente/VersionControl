42325c7f903284abfda2594542e7c150677f6e44
Alex Levente Socosan <alex.socosan@stud.uni-corvinus.hu>
2021-10-27 12:47:09 +0200

    Megjelenítés

--- Soap/Soap/Form1.cs
+++ Soap/Soap/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace Soap
@@ -21,6 +22,7 @@ namespace Soap
             InitializeComponent();
             Webszolghívása();
             dataGridView1.DataSource = Rates;
+            Megjelenítés();
         }
         private void Webszolghívása()
         {
@@ -58,6 +60,24 @@ namespace Soap
                     rd.Value = value / unit;
                 }
             }
+        }
+        private void Megjelenítés()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
