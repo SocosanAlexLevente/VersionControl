1d5fb6884917f8eb7cfdbd8e509801a0f75cc18b
Alex Levente Socosan <alex.socosan@stud.uni-corvinus.hu>
2021-11-08 11:33:47 +0100

    Toy

---
+++ Week08/Week08/Abstractions/Toy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Week08.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Height = 50;
+            Width = 50;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        protected abstract void DrawImage(Graphics g);
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
