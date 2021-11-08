c33478db4eccda7d4de3ffa81682d389b86640f4
Alex Levente Socosan <alex.socosan@stud.uni-corvinus.hu>
2021-11-08 12:23:49 +0100

    Termékválasztás

--- Week08/Week08/Form1.cs
+++ Week08/Week08/Form1.cs
@@ -16,10 +16,15 @@ namespace Week08
     {
         List<Toy> _toys = new List<Toy>();
         private IToyFactory _factory;
+        Toy _nextToy;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set 
+            {
+                _factory = value;
+                DisplayNext();    
+            }
         }
         public Form1()
         {
@@ -52,6 +57,27 @@ namespace Week08
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+            {
+                Controls.Remove(_nextToy);
+            }
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left + label1.Width / 2 - _nextToy.Width / 2;
+            Controls.Add(_nextToy);
         }
     }
 }
