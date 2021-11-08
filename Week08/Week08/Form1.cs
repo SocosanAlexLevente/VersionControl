43c6bf88e41173172439e16cbef670f6056faaa7
Alex Levente Socosan <alex.socosan@stud.uni-corvinus.hu>
2021-11-08 12:47:16 +0100

    labda színezése

--- Week08/Week08/Form1.cs
+++ Week08/Week08/Form1.cs
@@ -66,7 +66,10 @@ namespace Week08
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
         }
         private void DisplayNext()
         {
