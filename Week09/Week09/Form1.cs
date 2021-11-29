62b05ee8dfba4a9994741dfe660259e58d7ea98f
Alex Levente Socosan <alex.socosan@stud.uni-corvinus.hu>
2021-11-29 15:24:08 +0100

    Véletlenszám generálás

--- Week09/Week09/Form1.cs
+++ Week09/Week09/Form1.cs
@@ -14,6 +14,7 @@ namespace Week09
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(9936);
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
